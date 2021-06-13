e70a39532dffb9f336aeab729a9151cafde4d7c8
Uzair Aslam <uzairaslam64@gmail.com>
2021-06-13 23:38:49 +0500

    Remoting Service Sample

---
+++ IHelloRemotingService/RemotingServiceHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+
+namespace RemotingServiceHost
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      HelloRemotingService.HelloRemotingService remotingService = new HelloRemotingService.HelloRemotingService();
+      TcpChannel tcpChannel = new TcpChannel(8080);
+      ChannelServices.RegisterChannel(tcpChannel);
+      RemotingConfiguration.RegisterWellKnownServiceType(typeof(HelloRemotingService.HelloRemotingService), "GetMessage", WellKnownObjectMode.Singleton);
+      Console.WriteLine("Remoting service is started at " + DateTime.Now);
+      Console.ReadLine();
+    }
+  }
+}
